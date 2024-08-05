247b6e795e198a4f1d7bd9adad886075082d117f
Manuel Rao <108485835+porfi2089@users.noreply.github.com>
2024-08-05 08:58:06 -0300

    Create image_crop.py

---
+++ machine vision/image_crop.py
@@ -0,0 +1,7 @@
+import cv2 as cv
+import math_func as mf
+
+# resources
+# https://stackoverflow.com/questions/6555629/algorithm-to-detect-corners-of-paper-sheet-in-photo
+# https://docs.opencv.org/4.x/dc/d0d/tutorial_py_features_harris.html
+
