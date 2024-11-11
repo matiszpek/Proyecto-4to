d39c734bf9a0f9b3a52f80fc3c97036c1a282237
Matias Mayans Kohon <163904866+MatiMay@users.noreply.github.com>
2024-11-11 19:14:43 -0300

    Progress in concave objects
    For convex objects 💯 but has limits in concave objects

--- kono/convex.py
+++ kono/convex.py
@@ -1,30 +1,31 @@
-from scipy.spatial import ConvexHull, Delaunay
-import matplotlib.pyplot as plt
+from typing import List
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.spatial import ConvexHull
 
-def convex(list):
-
-    points=np.array([list])
-    # Use the QJ option to joggle the input points
-    hull = ConvexHull(points, qhull_options='QJ')
-    delaunay = Delaunay(points, qhull_options='QJ')
-
-    # Check if each point is inside the convex hull
-    inside_hull = all(delaunay.find_simplex(points) >= 0)
-    print("All points are inside the convex hull:", inside_hull)
-
-    edges = list(zip(*points))
-
+#Fuincion que se fija si objeto es convexo
+def convex(vertices: List[List[float]]):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
-    for i in hull.simplices:
-        plt.plot(points[i,0], points[i,1], points[i,2], 'r-')
+    points = np.array(vertices)
+    hull = ConvexHull(points)
+    
+    # Plot the vertices
+    ax.plot(points[:, 0], points[:, 1], points[:, 2], 'bo')
+    
+    # Plot the edges of the convex hull
+    for simplex in hull.simplices:
+        simplex = np.append(simplex, simplex[0])  # Cycle back to the first point
+        ax.plot(points[simplex, 0], points[simplex, 1], points[simplex, 2], 'r-')
 
-    ax.plot(edges[0], edges[1], edges[2], 'bo') 
-
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_zlabel('z')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
 
     plt.show()
+    # Check if all points are on the convex hull
+    hull_vertices = set(hull.vertices)
+    all_points_on_hull = all(i in hull_vertices for i in range(len(points)))
+    
+    return all_points_on_hull
---
+++ pre.py
@@ -0,0 +1,184 @@
+from typing import List, Tuple
+from stl import mesh # type: ignore
+from scipy.spatial import ConvexHull, Delaunay
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np # type: ignore
+import matplotlib.pyplot as plt
+import os
+
+#Conseguimos la ruta del escritorio
+desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
+output_file = os.path.join(desktop_path, "Output.stl")
+
+#Funcion que crea un objeto stl 3D
+def create_3d_object(coordinates):
+    # Create a numpy array from the coordinates
+    vertices = np.array(coordinates)
+
+    # Create a mesh object
+    mesh_object = mesh.Mesh(np.zeros(vertices.shape[0], dtype=mesh.Mesh.dtype))
+    for i, vertex in enumerate(vertices):
+        mesh_object.vectors[i] = vertex
+    return mesh_object
+
+#Funcion que exporta un objeto stl
+def export_stl(mesh_object, filename):
+    # Export the mesh object as an STL file
+    mesh_object.save(filename)
+
+
+#Crear las caras de un objeto
+def crearCaras(objeto):
+    con, ver = objeto
+    caras=[]
+
+    for i in range(len(con)):
+        for j in range(len(ver)):
+            if j in con[i]:
+                for k in range(len(ver)):
+                    if k!=j and k in con[i]:
+                        caras.append([i,j,k])
+                        #print(con[i],con[j],con[k])
+
+                        if j not in con[k]:
+                            con[j].append(k)
+                        if k not in con[j]:
+                            con[k].append(j)
+                        
+                        if i in con[j]:
+                            con[j].remove(i)
+                        if i in con[k]:
+                            con[k].remove(i)
+    return caras 
+
+def crearCaras2(objeto):
+    con, ver = objeto
+    caras=[]
+    for i in range(len(con)):
+        for j in range(len(ver)):
+            if j in con[i]:
+                for k in range(len(ver)):
+                    if k!=j and k in con[i]:
+                        caras.append([i,j,k])
+                        #print(con[i],con[j],con[k])
+    return caras
+
+
+#Convertir los numeros de las conecciones en coordenadas
+def llamarIndice(lista, coords):
+    
+    out = []
+    for i in lista:
+        in1=coords[i[0]]
+        in2=coords[i[1]]
+        in3=coords[i[2]]
+        out.append([in1,in2,in3])
+        #print([in1,in2,in3])
+    return out
+
+#Sacar posibles repeticiones de las caras
+def sacarIteraciones(caras):
+    vistos= set()
+    resultado= []
+    for i in caras:
+        if frozenset(i) not in vistos:
+            resultado.append(i)
+            vistos.add(frozenset(i))
+    return resultado
+
+
+#Funcion que contiene los vertices y sus conecciones
+def figure():
+    """input("Vertices:\n")""" 
+    """input("\nConections:\n")"""
+    # Definimos los vértices de un tetraedro.
+    # Definimos los vértices de un octaedro.
+    vertices: List[List[float]] = [
+        
+        [0,0,-1],
+        [1,-1,0],
+        [1,0,0],
+        [0,0,0],
+        [0,1,0],
+        [-1,1,0],
+        [-1,-1,0]
+    ]
+    
+    # Definimos las conexiones (aristas) entre los vértices del octaedro.
+    connections = [
+        
+        [[0,0,-1],[0,0,-1],[0,0,-1]],
+        [[-1,-1,0],[1,0,0],[0,0,1]],
+        [[1,-1,0],[0,0,0],[0,0,1]],
+        [[1,0,0],[0,1,0],[0,0,1]],
+        [[0,0,0],[-1,1,0],[0,0,1]],
+        [[0,1,0],[-1,-1,0],[0,0,1]],
+        [[1,-1,0],[-1,1,0],[0,0,1]],
+]
+    return connections, vertices
+
+
+#Traducir las conecciones en vertices a numeros
+def translate(figure):
+    connections, vertices= figure
+    for ver in range(len(vertices)):
+        k=-1
+        for i in connections:
+            k+=1
+            for j in range(len(i)):
+                if vertices[ver]==connections[k][j]:
+                    connections[k][j]=ver
+    return connections, vertices
+
+
+#Fuincion que se fija si objeto es convexo
+def convex(vertices: List[List[float]]):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    points = np.array(vertices)
+    hull = ConvexHull(points)
+    
+    # Plot the vertices
+    ax.plot(points[:, 0], points[:, 1], points[:, 2], 'bo')
+    
+    # Plot the edges of the convex hull
+    for simplex in hull.simplices:
+        simplex = np.append(simplex, simplex[0])  # Cycle back to the first point
+        ax.plot(points[simplex, 0], points[simplex, 1], points[simplex, 2], 'r-')
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+    plt.show()
+    # Check if all points are on the convex hull
+    hull_vertices = set(hull.vertices)
+    all_points_on_hull = all(i in hull_vertices for i in range(len(points)))
+    
+    return all_points_on_hull
+
+
+if __name__=="__main__":
+
+    coneciones, vertices=translate(figure())
+
+    if convex(vertices):#La figura es convexa
+        print("La figura es convexa")
+        
+        object=llamarIndice(sacarIteraciones(crearCaras((coneciones, vertices))),vertices) #Crear las caras del objeto
+        object3D = create_3d_object(object) #Crear el objeto 3D
+        #export_stl(object3D, output_file) #Exportar el objeto
+        export_stl(object3D, "Output.stl")
+        
+        print("Objeto creado")
+        
+    else:
+        print("La figura no es convexa")
+
+        object=llamarIndice(sacarIteraciones(crearCaras2((coneciones, vertices))),vertices) #Crear las caras del objeto
+        object3D = create_3d_object(object) #Crear el objeto 3D
+        #export_stl(object3D, output_file) #Exportar el objeto
+        export_stl(object3D, "Output.stl")
+
+        print("Objeto creado")
